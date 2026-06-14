dc374d5b6f3ae8a2068ba43c5a1bb895cd17f718
Alexander Koblov <alexx2000@mail.ru>
2026-06-14 13:19:38 +0300

    ADD: Lazarus 4.8

--- src/lazarus.ts
+++ src/lazarus.ts
@@ -8,10 +8,14 @@ import * as fs from "fs";
 
 import { Cache } from "./cache";
 
-const StableVersion = "4.6";
+const StableVersion = "4.8";
 
 const pkgs: object = {
   win32: {
+    v4_8: {
+        laz64: "lazarus-4.8-fpc-3.2.2-win64.exe",
+        laz32: "lazarus-4.8-fpc-3.2.2-cross-i386-win32-win64.exe"
+    },
     v4_6: {
         laz64: "lazarus-4.6-fpc-3.2.2-win64.exe",
         laz32: "lazarus-4.6-fpc-3.2.2-cross-i386-win32-win64.exe"
@@ -34,6 +38,10 @@ const pkgs: object = {
     }
   },
   win64: {
+    v4_8: {
+        laz64: "lazarus-4.8-fpc-3.2.2-win64.exe",
+        laz32: "lazarus-4.8-fpc-3.2.2-cross-i386-win32-win64.exe"
+    },
     v4_6: {
         laz64: "lazarus-4.6-fpc-3.2.2-win64.exe",
         laz32: "lazarus-4.6-fpc-3.2.2-cross-i386-win32-win64.exe"
